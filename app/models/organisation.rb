db419363bf3811c59fc125d57d342d092632ee15
simon <simon@d006ec09-ee59-0410-91b6-ee83e8a2eb67>
2008-11-01 20:53:49 +0000

    git-svn-id: https://thats-software.com/svn/svn_demowatch/trunk/rails@128 d006ec09-ee59-0410-91b6-ee83e8a2eb67

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,7 +3,6 @@ class Organisation < ActiveRecord::Base
   has_many :users, :through => :organizers
   acts_as_taggable
 
-  validates_presence_of  :title
-  validates_length_of    :title, :within => 3..100
-  
+  validates_presence_of  :title, :message => 'Name fehlt'
+  validates_length_of    :title, :within => 3..100, :message => 'Name ist zu kurz'
 end
