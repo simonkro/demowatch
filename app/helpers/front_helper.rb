65da57ac61e98d1f471d07ac137825d03198e178
simon <simon@d006ec09-ee59-0410-91b6-ee83e8a2eb67>
2008-11-01 23:14:35 +0000

    usertags
    git-svn-id: https://thats-software.com/svn/svn_demowatch/trunk/rails@138 d006ec09-ee59-0410-91b6-ee83e8a2eb67

--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -1,12 +1,13 @@
 module FrontHelper
-  def tag_cloud(tags, classes)
+  def tag_cloud(tags, classes, user)
     return if tags.empty?
     
     max_count = tags.map(&:count).max.to_f
+    user_tags = Set.new(user && user.tags || [])
     
     tags.sort_by{|tag| tag.name}.each do |tag|
       index = ((tag.count / max_count) * (classes.size - 1)).round
-      yield tag, classes[index]
+      yield tag, classes[index] + " #{'usertag' if user_tags.include? tag}"
     end
   end
 end
