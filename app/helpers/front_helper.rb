b62de3329b406217cfd925d6d22cc3d9d94fc26d
simon <simon@d006ec09-ee59-0410-91b6-ee83e8a2eb67>
2008-11-01 23:26:27 +0000

    cosmetic changes
    git-svn-id: https://thats-software.com/svn/svn_demowatch/trunk/rails@139 d006ec09-ee59-0410-91b6-ee83e8a2eb67

--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -5,7 +5,7 @@ module FrontHelper
     max_count = tags.map(&:count).max.to_f
     user_tags = Set.new(user && user.tags || [])
     
-    tags.sort_by{|tag| tag.name}.each do |tag|
+    tags.sort_by(&:name).each do |tag|
       index = ((tag.count / max_count) * (classes.size - 1)).round
       yield tag, classes[index] + " #{'usertag' if user_tags.include? tag}"
     end
