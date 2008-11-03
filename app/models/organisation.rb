ab49e0e879194455d35806dd95e92953c59d4e4e
simon <simon@d006ec09-ee59-0410-91b6-ee83e8a2eb67>
2008-11-03 19:48:45 +0000

    Confirmation
    git-svn-id: https://thats-software.com/svn/svn_demowatch/trunk/rails@205 d006ec09-ee59-0410-91b6-ee83e8a2eb67

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,10 +1,13 @@
 class Organisation < ActiveRecord::Base
+  attr_accessor :confirm
+
   acts_as_taggable
   acts_as_bookmarkable
 
   has_many :organizers, :dependent => :destroy
   has_many :users, :through => :organizers
 
-  validates_presence_of  :title, :message => 'Name fehlt'
-  validates_length_of    :title, :within => 3..100, :message => 'Name ist zu kurz'
+  validates_presence_of   :title,   :message => 'Name fehlt'
+  validates_length_of     :title,   :within => 3..100, :message => 'Name ist zu kurz'
+  validates_acceptance_of :confirm, :message => 'Sie m&uuml;ssen die Bedingungen akzeptieren!'
 end
