9dc4e89a71a148e7b8c391b2db5fed88466efcd8
pat <pat@d006ec09-ee59-0410-91b6-ee83e8a2eb67>
2008-11-03 20:41:00 +0000

    Issue: 469
    git-svn-id: https://thats-software.com/svn/svn_demowatch/trunk/rails@213 d006ec09-ee59-0410-91b6-ee83e8a2eb67

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,23 +1,23 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'
+    @subject    += 'Bitte Dein Konto auf Demowatch.de aktivieren!'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://www.demowatch.eu/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
-    @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @subject    += 'Dein Konto auf Demowatch.de wurde aktiviert'
+    @body[:url]  = "http://www.demowatch.eu/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "no-reply@demowatch.eu"
+      @subject     = "[www.demowatch.eu] "
       @sent_on     = Time.now
       @body[:user] = user
     end
