523de0633708e31544b763f12b69ba48455838e2
marc <marc@d006ec09-ee59-0410-91b6-ee83e8a2eb67>
2008-11-05 23:54:26 +0000

    redirect_to_www_demowatch_de
    git-svn-id: https://thats-software.com/svn/svn_demowatch/trunk/rails@259 d006ec09-ee59-0410-91b6-ee83e8a2eb67

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   helper :all # include all helpers, all the time
 
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '3a5df53b34706fb7a4b7c45303d16813'
@@ -16,6 +17,18 @@ class ApplicationController < ActionController::Base
   
   rescue_from Authorization::PermissionDenied, :with => :permission_denied
   rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+  
+  
+  
+  before_filter :redirect_to_www_demowatch_de
+  
+  # zur hauptdomain umleiten
+  def redirect_to_www_demowatch_de
+    if not request.host.include?('localhost') && request.host.match( /^www\.demowatch\.de/i).nil?
+      redirect_to "http://www.demowatch.de" + request.path, :status=>:moved_permanently
+    end
+  end  
+  
 protected
   def permission_denied(exception)
     flash[:notice] = 'Du hast keine Berechtigung.'
