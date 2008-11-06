7386938e45421d7a91f5949eb0acd9cbd44a222e
simon <simon@d006ec09-ee59-0410-91b6-ee83e8a2eb67>
2008-11-06 21:48:16 +0000

    git-svn-id: https://thats-software.com/svn/svn_demowatch/trunk/rails@278 d006ec09-ee59-0410-91b6-ee83e8a2eb67

---
+++ test/unit/initiator_mailer_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class InitiatorMailerTest < ActionMailer::TestCase
+  tests InitiatorMailer
+  # replace this with your real tests
+  def test_truth
+    assert true
+  end
+end
