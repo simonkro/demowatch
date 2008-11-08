73f058a45545383ad5c1574857bbedbc12a1e5ba
simon <simon@d006ec09-ee59-0410-91b6-ee83e8a2eb67>
2008-11-08 23:41:37 +0000

    git-svn-id: https://thats-software.com/svn/svn_demowatch/trunk/rails@317 d006ec09-ee59-0410-91b6-ee83e8a2eb67

--- config/initializers/localisation_hacks.rb
+++ config/initializers/localisation_hacks.rb
@@ -19,6 +19,6 @@ module LocalizationSimplified
 end
 
 Time::DATE_FORMATS[:rfc822] = proc do |time|
-  time.strftime("#{Time::RFC2822_DAY_NAME[time.wday]}, %d #{Time::RFC2822_MONTH_NAME[time.mon]} %Y %H:%M:%S %z")
+  time.strftime("#{Time::RFC2822_DAY_NAME[time.wday]}, %d #{Time::RFC2822_MONTH_NAME[time.mon-1]} %Y %H:%M:%S %z")
 end
   
