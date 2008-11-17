7627ecf63bc9eab000cde44ee8d7b680d52e31ea
simon <simon@d006ec09-ee59-0410-91b6-ee83e8a2eb67>
2008-11-17 01:03:23 +0000

    50 items per page in organisation view
    git-svn-id: https://thats-software.com/svn/svn_demowatch/trunk/rails@351 d006ec09-ee59-0410-91b6-ee83e8a2eb67

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -13,7 +13,7 @@ class OrganisationsController < ApplicationController
   # GET /organisations
   # GET /organisations.xml
   def index
-    @organisations = Organisation.paginate :order => 'title', :page => params[:page], :per_page => 10
+    @organisations = Organisation.paginate :order => 'title', :page => params[:page], :per_page => 50
 
     respond_to do |format|
       format.html # index.html.erb
